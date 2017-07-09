a5796a1803a22e70a1fa37efdecc89e86ec639a3
draginman225 <draginman225>
2017-07-10 03:38:00 +0800

    branch test

--- src/index.js
+++ src/index.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
+import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyCW6EQ9QVL9iRon5QrkTWfx5NwpY98rluw';
+const API_KEY = '';
 
 class App extends Component {
   constructor(props) {
